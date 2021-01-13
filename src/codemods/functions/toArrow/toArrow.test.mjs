9b581b52d852d1b648940ef8d763f7339e77fd18
aneurysm <aneurysmjs@outlook.com>
2021-01-14 01:51:09 +0300

    feat(functions): add toArrow mode
    replace function expression to arrow function

---
+++ src/codemods/functions/toArrow/toArrow.test.mjs
@@ -0,0 +1,41 @@
+import { applyTransform } from 'jscodeshift/dist/testUtils';
+
+import toArrowMod from './toArrow.mjs';
+
+const transformOptions = {};
+
+describe('toArrow mod', () => {
+  it('replace function expression to arrow function', () => {
+    const source = `
+    const foo = function () {
+      api();
+      return 'bar';
+    };
+    `;
+
+    const output = `
+    const foo = () => {
+      api();
+      return 'bar';
+    };
+    `;
+
+    const expected = applyTransform(toArrowMod(), transformOptions, { source });
+    expect(output.trim()).toEqual(expected);
+  });
+
+  it('replace function expression to one liner arrow function', () => {
+    const source = `
+    const foo = function () {
+      return 'bar';
+    };
+    `;
+
+    const output = `
+    const foo = () => 'bar';
+    `;
+
+    const expected = applyTransform(toArrowMod(), transformOptions, { source });
+    expect(output.trim()).toEqual(expected);
+  });
+});
