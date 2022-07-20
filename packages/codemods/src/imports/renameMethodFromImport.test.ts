345b32dc86213678a476910dd9ca8a262fe638a7
aneurysm <aneurysmjs@outlook.com>
2022-07-20 23:37:27 +0200

    feat(@mods/codemods): add renameMethodFromImport

---
+++ packages/codemods/src/imports/renameMethodFromImport.test.ts
@@ -0,0 +1,42 @@
+// @ts-ignore
+import { applyTransform } from 'jscodeshift/dist/testUtils';
+
+import renameMethodFromImport from './renameMethodFromImport';
+
+const transformOptions = {};
+
+describe('rename method from import', () => {
+  it('rename ONLY methods from User', () => {
+    const source = `
+      import User from 'user-lib';
+      import employeeManager from 'employee-manager';
+
+      User.checkAddress(userData)
+
+      function another() {
+        User.checkAddress(userData);
+      }
+
+      employeeManager.checkAddress(userData)
+    `;
+
+    const output = `
+      import User from 'user-lib';
+      import employeeManager from 'employee-manager';
+
+      User.check(userData)
+
+      function another() {
+        User.check(userData);
+      }
+
+      employeeManager.checkAddress(userData)
+    `;
+
+    console.log('applyTransform', applyTransform);
+
+    const expected = applyTransform(renameMethodFromImport, transformOptions, { source });
+
+    expect(output.trim()).toEqual(expected);
+  });
+});
---
+++ packages/codemods/src/imports/renameMethodFromImport.ts
@@ -0,0 +1,30 @@
+import type { FileInfo, API, Identifier, Node } from 'jscodeshift';
+
+export const parser = 'ts';
+
+export default (fileInfo: FileInfo, api: API) => {
+  const j = api.jscodeshift;
+
+  const root = j(fileInfo.source)
+    .find(j.MemberExpression)
+    .filter((path) => {
+      return (path.node.property as Identifier).name === 'checkAddress';
+    })
+    .forEach((path) => {
+      const objName = (path.node.object as Identifier).name;
+      const scope = path.scope.lookup(objName);
+      const bindings = scope.getBindings()[objName];
+
+      bindings.forEach((binding) => {
+        const gp = binding.parent.parent.node;
+
+        if (binding.node.name === 'User' && gp.type === 'ImportDeclaration') {
+          if (gp.source.value === 'user-lib') {
+            (path.node.property as Identifier).name = 'check';
+          }
+        }
+      });
+    });
+
+  return root.toSource();
+};
