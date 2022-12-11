3eb559c8e95ba7858b00ad88dd2e37b119c9d010
aneurysm <aneurysmjs@outlook.com>
2022-12-11 13:39:04 +0100

    chore(typescript): add definition files for jscodeshift
    create a `types` folder to allocate definition files and point mods-collection's tsconfig types to
    that types folder

---
+++ types/jscodeshift/index.d.ts
@@ -0,0 +1,18 @@
+/**
+ * Add your own Type Definition to any Javascript 3rd party module
+ * @see https://medium.com/@ofir3322/add-your-own-type-definition-to-any-javascript-3rd-party-module-1fc6b11e6f10
+ */
+export {};
+
+declare module 'jscodeshift' {
+  /**
+   * jscodeshift does not exports `RecursiveMatchNode` so that's why is redefined heres
+   */
+  export type RecursiveMatchNode<T> =
+    | (T extends {}
+        ? {
+            [K in keyof T]?: RecursiveMatchNode<T[K]>;
+          }
+        : T)
+    | ((value: T) => boolean);
+}
