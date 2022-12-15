b85bb64476ef1208a578b53fc22627055d0aa8ff
aneurysm <aneurysmjs@outlook.com>
2022-12-15 08:30:34 +0100

    chore(mods-cli): add initial path resolving for packages

---
+++ packages/mods-cli/src/index.ts
@@ -0,0 +1,75 @@
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import { globbySync } from 'globby';
+import yargs from 'yargs/yargs';
+import createDebug from 'debug';
+
+/**
+ * `realpathSync` return the canonicalized absolute pathname.
+ *
+ * what is cannonical path?
+ * @link https://stackoverflow.com/a/12100378/5378393
+ */
+const appDirectory = fs.realpathSync(process.cwd());
+const build = './build';
+const pathToBuild = path.resolve(appDirectory, build);
+
+// @ts-ignore
+const debug = createDebug('@mods-cli');
+// @ts-ignore
+const log = console.log.bind(console);
+
+const result = yargs(process.argv.slice(2))
+  .usage('Usage: $0 [file pattern]')
+
+  .option('transformation', {
+    alias: 't',
+    type: 'string',
+    describe: 'Name or path of the transformation module',
+  })
+  .option('params', {
+    alias: 'p',
+    describe: 'Custom params to the transformation',
+  })
+  .demandOption('transformation')
+  .help().argv;
+
+// @ts-ignore
+const { _: files, transformation: transformationName } = result;
+
+console.log('files', files);
+
+async function main() {
+  const resolvedPaths = globbySync(files as string[]);
+
+  const pathToIndex = `${pathToBuild}/index.js`;
+
+  const tfm = await import(pathToIndex);
+
+  console.log('tfm', tfm);
+
+  console.log('resolvedPaths ', resolvedPaths);
+
+  console.log('transformationName', transformationName);
+
+  // log(`Processing ${resolvedPaths.length} files…`);
+
+  // for (const p of resolvedPaths) {
+  //   debug(`Processing ${p}…`);
+  //   const fileInfo = {
+  //     path: p,
+  //     source: fs.readFileSync(p).toString(),
+  //   };
+  //   try {
+  //     const result = runTransformation(fileInfo, transformationModule, params as object);
+  //     fs.writeFileSync(p, result);
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // }
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
--- packages/mods-collection/src/index.ts
+++ packages/mods-collection/src/index.ts
@@ -1,1 +1,16 @@
-export { default as makeComponent } from './makeComponent';
+import type { Transform, Parser } from 'jscodeshift';
+
+import * as makeComponent from './jsx/makeComponent.js';
+
+type JSTransformationModule = {
+  default: Transform;
+  parser?: string | Parser;
+};
+// @ts-ignore
+const transformationMap: {
+  [name: string]: JSTransformationModule;
+} = {
+  ...makeComponent,
+};
+
+export default transformationMap;
