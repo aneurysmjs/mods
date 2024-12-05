d12142d71f52fb69c4b9b41088a13d341e085216
aneurysmjs <aneurysmjs@outlook.com>
2024-12-05 11:24:50 +0100

    fix(typings): improve typings

--- packages/mods-makers/src/objects/makeObjectProperty.ts
+++ packages/mods-makers/src/objects/makeObjectProperty.ts
@@ -1,7 +1,7 @@
-import { property, identifier } from 'jscodeshift';
+import { property, identifier, type Property } from 'jscodeshift';
 
 export type PropertyValue = Parameters<typeof property>[2];
 
-export default function makeObjectProperty(key: string, value: PropertyValue) {
+export default function makeObjectProperty(key: string, value: PropertyValue): Property {
   return property('init', identifier(key), value);
 }
