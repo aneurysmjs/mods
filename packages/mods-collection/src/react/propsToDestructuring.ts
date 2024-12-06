4732f38c0456ec1669978981aec22de434f0de9d
aneurysmjs <aneurysmjs@outlook.com>
2024-12-07 00:21:44 +0100

    refactor(propsToDestructuring): improve typings

--- packages/mods-collection/src/react/propsToDestructuring.ts
+++ packages/mods-collection/src/react/propsToDestructuring.ts
@@ -1,7 +1,42 @@
-import type { FileInfo, API, Identifier, ASTPath, MemberExpression, Expression } from 'jscodeshift';
+import {
+  type FileInfo,
+  type API,
+  type Identifier,
+  type ASTPath,
+  type MemberExpression,
+  type Expression,
+  type Property,
+  type VariableDeclaration,
+} from 'jscodeshift';
 import { print } from '@mods/utils';
 
 type KeywordMap = Record<string, boolean>;
+
+/**
+ * @see https://github.com/benjamn/ast-types/blob/master/src/scope.ts#L7
+ */
+interface Scope {
+  path: ASTPath;
+  node: any;
+  isGlobal: boolean;
+  depth: number;
+  parent: any;
+  bindings: any;
+  types: any;
+  didScan: boolean;
+  declares(name: any): any;
+  declaresType(name: any): any;
+  declareTemporary(prefix?: any): any;
+  injectTemporary(identifier: any, init: any): any;
+  scan(force?: any): any;
+  getBindings(): Record<string, []>;
+  getTypes(): any;
+  lookup(name: string): Scope | null;
+  lookupType(name: string): {
+    getTypes: Scope['getTypes'];
+  };
+  getGlobalScope(): Scope;
+}
 
 const KEYWORDS =
   'this function if return var else for new in typeof while case break try catch delete throw switch continue default instanceof do void finally with debugger implements interface package private protected public static class enum export extends import super true false null abstract boolean byte char const double final float goto int long native short synchronized throws transient volatile';
@@ -29,20 +64,20 @@ const getPropertyName = (path: ASTPath<MemberExpression>) => {
  */
 export default function propsToDestucturing(file: FileInfo, api: API) {
   const j = api.jscodeshift;
-  const { statement } = j.template;
 
   return j(file.source)
     .find(j.FunctionExpression)
-    .replaceWith((fePath) => {
-      const root = j(fePath.value);
+    .replaceWith((functionExpressionPath) => {
+      const root = j(functionExpressionPath.value);
 
       const variablesToReplace: Record<string, boolean> = {};
 
-      // @ts-ignore Figure out if the variable was defined from props, so that we can re-use that definition.
-      const isFromProps = (name: string, resolvedScope) => {
+      // Figure out if the variable was defined from props, so that we can re-use that definition.
+      const isFromProps = (name: string, resolvedScope: Scope): boolean => {
+        // @ts-ignore
+        console.log('resolvedScope.getBindings()[name]', resolvedScope.getBindings()[name]);
 
-        // @ts-ignore -> fix when`getBindings` has typings
-         return resolvedScope.getBindings()[name].every((p) => {
+        return resolvedScope.getBindings()[name].every((p) => {
           const decl = j(p).closest(j.VariableDeclarator);
           // What happens when our VariableDeclarator is too high up the parent AST?
 
@@ -63,6 +98,8 @@ export default function propsToDestucturing(file: FileInfo, api: API) {
 
           // Check for the case where it could be aliased (i.e.) { baz: foo } = this.props;
           // In this case, we won't do a substitution.
+          // @ts-expect-error
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
           if (p.parentPath.value.type == 'Property' && p.parentPath.value.key.name !== name) {
             return false;
           }
@@ -81,16 +118,20 @@ export default function propsToDestucturing(file: FileInfo, api: API) {
             property: { name: 'props' },
           },
         })
-        .filter((mePath) => {
-          print(j(mePath));
-          // console.log('mePath.scope', mePath.scope);
-          // mePath -> this.props.bar
-          const resolvedScope = mePath.scope.lookup((mePath.value.property as Identifier).name);
+        .filter((memberExpressionPath) => {
+          const scope = memberExpressionPath.scope as Scope;
+
+          const resolvedScope = scope.lookup(
+            (memberExpressionPath.value.property as Identifier).name,
+          );
 
           // If the scope is null, that means that this property isn't defined in the scope yet,
           // and we can use it. Otherwise, if it is defined, we should see if it was defined from `this.props`
           // if none of these cases are true, we can't do substitution.
-          return resolvedScope == null || isFromProps(getPropertyName(mePath), resolvedScope);
+          return (
+            resolvedScope == null ||
+            isFromProps(getPropertyName(memberExpressionPath), resolvedScope)
+          );
         })
         // Ensure that our substitution won't cause us to define a keyword, i.e. `this.props.while` won't
         // get converted into `while`.
@@ -101,7 +142,7 @@ export default function propsToDestucturing(file: FileInfo, api: API) {
         // if it's not already defined.
         .forEach((p) => {
           // Is this prop already defined somewhere else.
-          if (!p.scope.lookup((p.value as Identifier).name)) {
+          if (!(p.scope as Scope).lookup((p.value as Identifier).name)) {
             variablesToReplace[(p.value as Identifier).name] = true;
           }
         });
@@ -117,7 +158,7 @@ export default function propsToDestucturing(file: FileInfo, api: API) {
 
       // We have no properties to inject, so we can bail here.
       if (!properties.length) {
-        return fePath.value;
+        return functionExpressionPath.value;
       }
 
       // See if we already have a VariableDeclarator like { a, b, c } = this.props;
@@ -133,21 +174,25 @@ export default function propsToDestucturing(file: FileInfo, api: API) {
       if (propDefinitions.size()) {
         const nodePath = propDefinitions.paths()[0];
         const node = nodePath.value;
+
         // @ts-ignore
-        const newPattern = j.objectPattern(node.id.properties.concat(properties));
+        const newPattern = j.objectPattern((node.id.properties as Property[]).concat(properties));
+
         nodePath.replace(j.variableDeclarator(newPattern, node.init));
-        return fePath.value;
+        return functionExpressionPath.value;
       }
 
       // Otherwise, we'll have to create our own, as none were suitable for use.
       // Create the variable definition `const { xyz } = this.props;`
-      const decl = statement`const { ${properties} } = this.props;`;
+      const decl = j.template
+        .statement`const { ${properties} } = this.props;` as VariableDeclaration;
 
       // Add the variable definition to the top of the function expression body.
       return j.functionExpression(
-        fePath.value.id,
-        fePath.value.params,
-        j.blockStatement([decl].concat(fePath.value.body.body)),
+        functionExpressionPath.value.id,
+        functionExpressionPath.value.params,
+        // @ts-ignore
+        j.blockStatement([decl].concat(functionExpressionPath.value.body.body)),
       );
     })
     .toSource();
